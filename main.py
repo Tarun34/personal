99f752a096fb90aa2e725434fd8eaf240b6950e1
Tarun kumar <65671215+Tarun34@users.noreply.github.com>
2022-02-14 13:44:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ with col2:
         for i in range(5):
             st.balloons()
             st.balloons()
-        pygame.init()
-        pygame.mixer.music.load("Welcome.MP3")
-        pygame.mixer.music.play(loops=2)
+        pygm = pygame.mixer.init()
+        pygm.mixer.music.load("Welcome.MP3")
+        pygm.mixer.music.play(loops=2)
         st.write("<p style = 'color:Red; font-weight: bold'> Happy Valentine Day Baby  😍 </p>",unsafe_allow_html=True)
         st.write("Yeh Apka phla gift "
                  "Apke ke liye ek choti se video bhi bnai h maine khud niche ek or button use press krtey hi apko show hogi")
