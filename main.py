cb8899fa0547c5826ffba17b639dd6b30e36298e
Tarun kumar <65671215+Tarun34@users.noreply.github.com>
2022-02-14 13:03:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pygame
+st.set_page_config(page_title="Happy Valentines Day",layout="centered")
+col1,col2 = st.columns([1,1])
+with col1:
+    st.image("images1.png")
+with col2:
+    st.header(" ")
+    st.header(" ")
+    st.header(" ")
+    if st.button("Press the Button"):
+        for i in range(5):
+            st.balloons()
+            st.balloons()
+        pygame.mixer.init()
+        pygame.mixer.music.load("Welcome.mp3")
+        pygame.mixer.music.play(loops=2)
+        st.write("<p style = 'color:Red; font-weight: bold'> Happy Valentine Day Baby  😍 </p>",unsafe_allow_html=True)
+        st.write("Yeh Apka phla gift "
+                 "Apke ke liye ek choti se video bhi bnai h maine khud niche ek or button use press krtey hi apko show hogi")
+Date = st.date_input("Btao hum phli bar maine apko kb propose kiya the Date")
+if st.button("submit"):
+    if str(Date) == "2021-10-29":
+            st.video("mai.mp4")
+
+
